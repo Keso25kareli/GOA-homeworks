4cc9a7297ca6f0a0c55175172a60847206ecb790
Keso25kareli <kareli25keso@gmail.com>
2024-11-16 15:15:32 +0400

    fifth commit

---
+++ level9/class/task2.py
@@ -0,0 +1,15 @@
+name="keso"
+surname="kareli"
+message=name  + surname
+print(message)
+
+ 
+number1=(89)
+number2=(67)
+result= str(number1) + str(number2)
+print(result)
+
+name="lara"
+age=27
+massage=name + str(age)
+print(massage)
