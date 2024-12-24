9d3966bc0823ef2417800c717d1b747eac7ac9d5
Keso25kareli <kareli25keso@gmail.com>
2024-12-24 23:40:37 +0400

    eleventh commit

---
+++ level19/home/task1.py
@@ -0,0 +1,10 @@
+numbers = []
+
+
+for i in range(10):
+    number = int(input(f" ({i+1}/10): "))
+    numbers.append(number)
+
+
+print(":", numbers)
+
---
+++ level21/class/task3.py
@@ -0,0 +1,5 @@
+queue=['john', 'amy', 'bob', 'adam']
+new_person=input("enter your name: ")
+queue.append (new_person)
+print(queue)
+#პას:['john', 'amy', 'bob', 'adam', 'keso']
