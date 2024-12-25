7a12055b40e41a67fd5991bd3ad56f773b857e61
Keso25kareli <kareli25keso@gmail.com>
2024-12-25 09:31:16 +0400

    eleventh commit

--- level19/home/task1.py
+++ level19/home/task1.py
@@ -1,10 +1,7 @@
-numbers = []
-
-
+numbers =[]
+ 
 for i in range(10):
-    number = int(input(f" ({i+1}/10): "))
+    number = int(input(f"({i+1}/10)"))
     numbers.append(number)
 
-
-print(":", numbers)
-
+    print(":" , numbers)
