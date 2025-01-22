9d1b98b813cf8011ba75493979ca63a66d47bc92
Keso25kareli <kareli25keso@gmail.com>
2025-01-22 18:44:56 +0400

    eighteenth commit

---
+++ level29/class/task2.py
@@ -0,0 +1,7 @@
+queue=['john,' 'amy', 'bob','adam']
+#გვინდა რომ დავამატოთ ახალი სახელი
+new_item=input("enter name to add to queue: ")
+#ზემოოთ შევიყვანთ რომელი სახელის დამატებაც გვინდა
+queue.append(new_item)
+#შემდეგ append ფუნქციას ვიყენებთ რომ დაპრინტვის როს ეს ახალი სახელი დაემატოს
+print(queue)
---
+++ level29/home/task2.py
@@ -0,0 +1,9 @@
+fruits =['apple' , 'banana', 'cherry', 'date', 'elderberry']
+print(fruits)
+print(min(fruits))
+print(max(fruits))
+fruits.append('fig')
+print(fruits)
+fruits.remove('banana')
+print(fruits)
+
