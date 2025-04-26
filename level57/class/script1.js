3ec98c66396b73b054a1c65dbcf626f6d965c813
Keso25kareli <kareli25keso@gmail.com>
2025-04-26 20:59:23 +0400

    commit

---
+++ level57/class/script1.js
@@ -0,0 +1,5 @@
+let name = prompt('enter name: ')
+let lastName = prompt ('enter surname: ')
+let proffesion = ('entyer profesion :')
+
+console.log("Hello, my name is + name, + my last name is + lastName, my profession is + proffesion");
---
+++ level57/class/script2.js
@@ -0,0 +1,4 @@
+let totalItems = 200;
+let itemsPerBox = 7;
+
+console.log(200 % 7);
