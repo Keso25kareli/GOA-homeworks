ce3d1403f2a7e0414afcabee456c7e6b45dfa48d
Keso25kareli <kareli25keso@gmail.com>
2025-05-10 12:33:56 +0400

    commit

---
+++ level59/class/task1.js
@@ -0,0 +1,17 @@
+let subjectOne = number(prompt("enter your point in first subject:"))
+let subjectTwo = number(prompt("enter your point in second subject:"))
+let subjectThree = number(prompt("enter your point in therth subject:"))
+
+let point = (subjectOne + subjectTwo + subjectThree / 3)
+
+if (point >= 90 <= 100) {
+    console.log('A')
+} else if (age >=70 <=80 ) {
+    console.log('B')
+} else if (age >= 50 <=65){
+    console.log('C')
+} else if (age >= 25 <=50){
+    console.log('D')
+}else (age < 25) 
+    console.log('წადი ისწავლე და მერე მოდი')
+
